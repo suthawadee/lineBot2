4fbbf28767c76b6e5fcb25b2c4bb3a459eb4da4b
suthawadee <52515393+suthawadee@users.noreply.github.com>
2019-07-10 15:47:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     a=os.environ['Authorization']
-    return "นายอาคม สุวรรณประเสริฐ เลขที่ 0 ชั้น ม.4/3"
+    return "นางสาวสุทาวดี หมอโอสถชั้นม.4/6 เลขที่27"
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
